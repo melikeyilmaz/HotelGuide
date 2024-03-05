abb0d8148ec61cc887f1ec4b102a6e5c40c26eea
melikeyilmaz <47854574+melikeyilmaz@users.noreply.github.com>
2024-03-05 12:23:03 +0300

    Entity kısmında güncelleme yapıldı.

---
+++ HotelService.Data/Entities/Responsibility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelService.Data.Entities
+{
+    public class Responsibility
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public Guid HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+
+    }
+}
