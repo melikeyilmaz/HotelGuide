7eeacbab7fe54c9e904a9b1131813f3983102f5e
melikeyilmaz <47854574+melikeyilmaz@users.noreply.github.com>
2024-03-06 21:25:13 +0300

    Rapor kısmı güncellemeler.

--- HotelService.API/Program.cs
+++ HotelService.API/Program.cs
@@ -1,12 +1,22 @@
+using HotelService.Data.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<Context>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
